46036c4d69f2cc914831b030736b754d626bea2f
Peter Borkuti <peter@aspire>
2016-11-18 05:30:42 +0100

    cycle time

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,11 @@ NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE);
 NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
 
 //Define Variables we'll be connecting to
-double Setpoint, Input, Output;
+double Setpoint = 0, Input, Output;
 
 //Specify the links and initial tuning parameters
-double Kp=2, Ki=5, Kd=1;
-PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, REVERSE);
+double Kp=0.1, Ki=0, Kd=0;
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 const unsigned int min_echo = 100;
 const unsigned int max_echo = 700;
@@ -35,19 +35,23 @@ const unsigned int center = 440;
 void setup() {
   Serial.begin(115200);
   myPID.SetOutputLimits(-255.0, 255.0);
+  myPID.SetSampleTime(5);
 }
 
 void emergencyStop() {
     motors.stop();
     myPID.SetMode(MANUAL);
+    myPID.Compute();
     //Serial.println("STOP");
 }
 
 float echo1, echo2;
+unsigned long m;
 
 void loop() {
-    echo1 = sonar1.ping_median(3);
-    echo2 = sonar2.ping_median(3);
+    m= millis();
+    echo1 = sonar1.ping();
+    echo2 = sonar2.ping();
 
     if (echo1 == 0 || echo2 == 0 || echo1 < min_echo || echo2 < min_echo) {
         emergencyStop();
@@ -56,53 +60,22 @@ void loop() {
 
     myPID.SetMode(AUTOMATIC);
 
-    Input = echo1 - echo2;
+    Input = echo2 - echo1;
+    /*
     Serial.print(" ECHOS:");
     Serial.print(echo1);
     Serial.print(",");
     Serial.println(echo2);
-
+    */
     myPID.Compute();
 
     motors.setPWM(Output);
-}
+    Serial.print(millis() - m);
+    Serial.print(";");
+    Serial.print(Input);
+    Serial.print(";");
+    Serial.println(Output);
 
-void dcmotor_test() {
-/*
-    Serial.println("Left motor forward");
-    lMotor.setPWM(1000);
-    delay(1000);
-    lMotor.stop();
-
-    Serial.println("Right motor forward");
-    rMotor.setPWM(1000);
-    delay(1000);
-    rMotor.stop();
-
-    Serial.println("Left motor bw");
-    lMotor.setPWM(-1000);
-    delay(1000);
-    lMotor.stop();
-
-    Serial.println("Right motor bw");
-    rMotor.setPWM(-1000);
-    delay(1000);
-    rMotor.stop();
-
-    Serial.println("Both motor fw");
-    lMotor.setPWM(1000);
-    rMotor.setPWM(1000);
-    delay(1000);
-    rMotor.stop();
-    lMotor.stop();
-
-    Serial.println("Both motor bw");
-    lMotor.setPWM(-1000);
-    rMotor.setPWM(-1000);
-    delay(1000);
-    rMotor.stop();
-    lMotor.stop();
-*/
 }
 
 #endif
